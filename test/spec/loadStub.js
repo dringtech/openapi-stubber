0b86f0172009040885d1d548d4add5a3f0585633
Giles Dring <gilesdring@gmail.com>
2019-06-06 09:13:50 +0100

    Implement the loadStub function

---
+++ test/spec/loadStub.js
@@ -0,0 +1,22 @@
+const sinon = require('sinon');
+const proxyquire = require('proxyquire');
+const chai = require('chai');
+
+const { expect } = chai;
+
+describe('lib/loadStub', () => {
+  let fakeStartStub, loadStub;
+
+  const fakeSpec = {};
+
+  beforeEach(() => {
+    fakeStartStub = sinon.fake();
+    const lib = proxyquire('../../lib', { './stub': fakeStartStub });
+    ({ loadStub } = lib);
+  });
+
+  it('should start a stub', () => {
+    loadStub('NAME', fakeSpec);
+    return expect(fakeStartStub).to.have.been.calledWith('NAME', fakeSpec);
+  });
+});
