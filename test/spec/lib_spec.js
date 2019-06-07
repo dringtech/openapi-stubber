e5a1167eaf5c7af7861accb231de7e49c0672f90
Giles Dring <gilesdring@gmail.com>
2019-06-07 12:06:29 +0100

    Move spec for main lib into single file

--- test/spec/lib_spec.js
+++ test/spec/lib_spec.js
@@ -1,4 +1,7 @@
 const chai = require('chai');
+const sinon = require('sinon');
+const proxyquire = require('proxyquire');
+
 const { expect } = chai;
 
 describe('lib', () => {
@@ -10,5 +13,31 @@ describe('lib', () => {
   it('should provide a tearDown function', () => {
     const lib = require('../../lib');
     expect(lib).to.have.property('tearDown').which.is.a('function');
+  });
+
+  describe('#loadStub', () => {
+    let fakeStartStub, loadStub, fakeOptions;
+
+    const fakeError = new Error('FAKE ERROR');
+
+    beforeEach(() => {
+      fakeOptions = { fail: false };
+      fakeStartStub = sinon.fake(async (options) => {
+        const { fail = false } = options;
+        if (fail) throw fakeError;
+      });
+      const lib = proxyquire('../../lib', { './stub': fakeStartStub });
+      ({ loadStub } = lib);
+    });
+
+    it('should start a stub', async () => {
+      await loadStub(fakeOptions);
+      return expect(fakeStartStub).to.have.been.calledWith(fakeOptions);
+    });
+
+    it('should rethrow errors', () => {
+      fakeOptions.fail = true;
+      return expect(loadStub(fakeOptions)).to.be.rejectedWith(fakeError);
+    });
   });
 });
