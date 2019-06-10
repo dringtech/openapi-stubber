6d52a298f8baca45ad4bfbf1f846af9aadbc31b4
Giles Dring <giles@dringtech.com>
2019-06-10 15:55:16 +0100

    Add logging to stub setup

--- test/spec/stub_spec.js
+++ test/spec/stub_spec.js
@@ -5,7 +5,7 @@ const proxyquire = require('proxyquire');
 const { expect } = chai;
 
 describe('lib/stub', () => {
-  let stub, fakeGetMiddleware, fakeExpress, fakeUse, fakeListen, options, fakeMiddleware, fakeGetLogger;
+  let stub, fakeGetMiddleware, fakeExpress, fakeUse, fakeListen, options, fakeMiddleware, fakeGetLogger, fakeInfoLogger, fakeErrorLogger;
 
   beforeEach(() => {
     options = {
@@ -13,15 +13,21 @@ describe('lib/stub', () => {
       spec: 'VALID',
       port: 8080,
     };
+    fakeInfoLogger = sinon.fake();
+    fakeErrorLogger = sinon.fake();
     fakeGetLogger = sinon.fake((name) => {
       if (name === 'NOT_INITED') throw new Error('Logger not inialised - call setupLogger');
       return {
+        logger: {
+          info: fakeInfoLogger,
+          error: fakeErrorLogger,
+        },
         requestLogger: 'FAKED REQUEST LOGGER',
         errorLogger: 'FAKED ERROR LOGGER',
       };
     });
     fakeMiddleware = (req, res, next) => next();
-    fakeGetMiddleware = sinon.fake(async spec => {
+    fakeGetMiddleware = sinon.fake(async (spec) => {
       if (spec === 'INVALID') throw new Error('FAKE GET MIDDLEWARE ERROR');
       return fakeMiddleware;
     });
@@ -72,6 +78,10 @@ describe('lib/stub', () => {
   it('should instantiate a logger if the logger is setup', async () => {
     await stub(options);
     expect(fakeGetLogger).to.have.been.calledWith('NAME');
+    expect(fakeInfoLogger).to.have.been.calledWith('Registering NAME stub with VALID');
+    expect(fakeInfoLogger).to.have.been.calledWith('Mock for NAME listening on 8080');
+    expect(fakeInfoLogger).to.have.callCount(2);
+    expect(fakeErrorLogger).to.have.not.been.called;
     expect(fakeUse).to.have.been.calledWith('FAKED REQUEST LOGGER');
     return expect(fakeUse).to.have.been.calledWith('FAKED ERROR LOGGER');
   });
@@ -79,6 +89,8 @@ describe('lib/stub', () => {
   it('should not instantiate a logger if the logger is not setup', async () => {
     options.name = 'NOT_INITED';
     await stub(options);
+    expect(fakeInfoLogger).to.have.not.been.called;
+    expect(fakeErrorLogger).to.have.not.been.called;
     expect(fakeUse).to.have.not.been.calledWith('FAKED REQUEST LOGGER');
     return expect(fakeUse).to.have.not.been.calledWith('FAKED ERROR LOGGER');
   });
@@ -86,5 +98,29 @@ describe('lib/stub', () => {
   it('should throw exceptions from openApi#getMiddleware', async () => {
     options.spec = 'INVALID';
     return expect(stub(options)).to.be.rejectedWith('FAKE GET MIDDLEWARE ERROR');
+  });
+
+  it('should log errors if logger setup', async () => {
+    options.spec = 'INVALID';
+    try {
+      await stub(options);
+    } catch (error) {
+      expect(error).to.have.property('message', 'FAKE GET MIDDLEWARE ERROR');
+    }
+    expect(fakeInfoLogger).to.have.callCount(1);
+    expect(fakeErrorLogger).to.have.callCount(1);
+    return expect(fakeErrorLogger).to.have.been.calledWith(sinon.match({ message: 'FAKE GET MIDDLEWARE ERROR' }));
+  });
+
+  it('should not log errors if logger not setup', async () => {
+    options.name = 'NOT_INITED';
+    options.spec = 'INVALID';
+    try {
+      await stub(options);
+    } catch (error) {
+      expect(error).to.have.property('message', 'FAKE GET MIDDLEWARE ERROR');
+    }
+    expect(fakeInfoLogger).to.have.not.been.called;
+    return expect(fakeErrorLogger).to.have.not.been.called;
   });
 });
